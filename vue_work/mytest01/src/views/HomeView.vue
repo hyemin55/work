cd253be0fff97f431b685749a21cd9f5cd4e596b
hyemin55 <whgpals641@naver.com>
2024-10-08 01:48:14 +0900

    241008

--- vue_work/mytest01/src/views/HomeView.vue
+++ vue_work/mytest01/src/views/HomeView.vue
@@ -1,9 +1,30 @@
+<template>
+  <div>
+<nav>
+  <h1>LOGO</h1>
+  <ul>
+    <li>Perfume</li>
+    <li>Diffuser</li>
+    <li>Candle</li>
+  </ul>
+  <ul>
+    <li>로그인</li>
+             <li>회원가입</li>
+  </ul>
+</nav>
+<div>
+<ul>
+  <li>검색창</li>
+  <li>장바구니</li>
+</ul>
+</div>
+  </div>
+</template>
+
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style lang="scss" scoped>
+
+</style>
--- vue_work/mytest01/src/views/hi.vue
+++ vue_work/mytest01/src/views/hi.vue
@@ -1,13 +1,35 @@
 <template>
     <div>
-<h1>hi page</h1>
+        <div id="A" @click="divA">
+            <div id="B" @click="divB">
+                <div id="c" @click="divC">
+                    <h1>button</h1>
+                    <button @click.stop="btnClick">button</button>
+                    <a href="http://www.naver.com" @click.prevent="">naver</a>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script setup>
-
+const divA = ()=>{
+    console.log('divA');
+}
+const divB = ()=>{
+    console.log('divB');
+}
+const divC = ()=>{
+    console.log('divC');
+}
+const btnClick = ()=>{
+    console.log('btnClick');
+}
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+div{ width: 60%; padding: 2rem;}
+#A{ background-color: #ccc}
+#B{ background-color: #999}
+#C{ background-color: #000; height: 100;;}
 </style>
